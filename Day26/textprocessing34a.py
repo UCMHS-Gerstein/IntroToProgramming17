880a3072641ad29dd6a0d41817af75d5c71a68dc
Jackie <gersteinj@gmail.com>
2017-11-30 09:36:37 -0500

    getting ready for 34b

--- Day26/textprocessing34a.py
+++ Day26/textprocessing34a.py
@@ -1,18 +1,25 @@
-text = "I hope that in this year to come, you make mistakes. Because if you are making mistakes, then you are making new things, trying new things, learning, living, pushing yourself, changing yourself, changing your world. You're doing things you've never done before, and more importantly, you're doing something."
+# initial uncleaned string
+text = "I hope that in this year to come, you make mistakes. Because if you are making mistakes, then you are making new things, trying new things, learning, living, pushing yourself, changing yourself, changing your world. You're doing things you've never done before, and more importantly, you're doing something.".lower()
 
-cleaned_text = text.lower()
+def text_cleaner(text_to_clean):
+    """clean text of undesired characters and return a cleaned string"""
+    # create empty string to hold cleaned text
+    cleaned_text = ''
+    # for each character in the string, copy over only the
+    # non-forbidden characters into the new string
+    for letter in text_to_clean.lower():
+        if letter not in '.,':
+            cleaned_text += letter
+    return cleaned_text
 
-split_text = cleaned_text.split()
+# split text and assign it to split_text
+split_text = text_cleaner("""Each January at the Kickoff, a new, challenging game is introduced. These exciting competitions combine
+the practical application of science and technology with the fun, intense energy and excitement of a
+championship-style sporting event. Teams are encouraged to display Gracious Professionalism® and to
+help other teams and cooperate while competing. This is known as Coopertition®.
+""").split()
+# sort the list
+split_text.sort()
 
-print(sorted(split_text))
-
-for item in split_text:
-    print(item)
-
-# words = {'cat': 5}
-words = {}
-# words['cat'] = 5
-
-temp_words = {'hope': 1, 'year': 3}
-
-print('hope' in temp_words.keys())
+# print the list
+print(split_text)
--- Day26/textprocessing34b.py
+++ Day26/textprocessing34b.py
@@ -1,16 +1,16 @@
-import string
+text = "I hope that in this year to come, you make mistakes. Because if you are making mistakes, then you are making new things, trying new things, learning, living, pushing yourself, changing yourself, changing your world. You're doing things you've never done before, and more importantly, you're doing something.".lower()
 
-text = "I hope that in this year to come, you make mistakes. Because if you are making mistakes, then you are making new things, trying new things, learning, living, pushing yourself, changing yourself, changing your world. You're doing things you've never done before, and more importantly, you're doing something."
+def clean_text(text_to_clean): 
+    """convert text_to_clean to lowercase and
+    strip out punctuation. print the result"""
+    for n in text_to_clean.lower():
+        if n not in ',.':
+            print(n, end='')
 
-cleaned_text = ''
+clean_text(text)
 
-for letter in text:
-    if letter not in string.punctuation:
-        cleaned_text += letter
+# split_text = cleaned_text.split()
 
+# split_text.sort()
 
-split_text = cleaned_text.split()
-
-split_text.sort()
-
-print(split_text)
+# print(split_text)
